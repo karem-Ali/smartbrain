b40825e8fa1060930ef3cd5ed78567881befea60
karem-Ali <karem.phones2018@gmail.com>
2020-04-20 04:34:33 +0200

    Updates

---
+++ precache-manifest.97a70a8c550bf1b522ae8b7a2db7caa6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b065aec650bc650849352f396faa86",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "cb37784cd90e2ca81b38",
+    "url": "/smartbrain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "650811531962845d8ecc",
+    "url": "/smartbrain/static/css/main.d0d07224.chunk.css"
+  },
+  {
+    "revision": "cb37784cd90e2ca81b38",
+    "url": "/smartbrain/static/js/2.c41c628c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smartbrain/static/js/2.c41c628c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650811531962845d8ecc",
+    "url": "/smartbrain/static/js/main.82f951c4.chunk.js"
+  },
+  {
+    "revision": "2c07235ce4f3a705697d",
+    "url": "/smartbrain/static/js/runtime-main.7377aab0.js"
+  },
+  {
+    "revision": "014560c03cbcab7f1adee1b2824cc5d6",
+    "url": "/smartbrain/static/media/brain.014560c0.svg"
+  }
+]);
